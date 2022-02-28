4e6f8c4ebee00f3d5ffd65a32822ae1fb5affea4
Bryan <balegria.gonz@gmail.com>
2022-02-27 20:57:28 -0500

    First Commit - Frontend

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"], // Indica el archivo que se va a renderizar y que se estarán creando archivos con la extension jsx.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
